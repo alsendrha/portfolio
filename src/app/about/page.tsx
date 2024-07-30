67c8ddbf5930cbf46fdabc904fd92184e9b942ac
Minyoung <alsendrha@naver.com>
2024-07-30 16:37:54 +0900

    어바웃 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import Menubar from "@/components/menu/Menubar";
-
-import Image from "next/image";
-import Project from "../project/page";
-import Sidebar from "@/components/menu/Sidebar";
-
 const Main = () => {
   return <></>;
 };
