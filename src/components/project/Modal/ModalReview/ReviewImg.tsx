496cf6f01af64d58f4fbf95d3f882fa0af3b0c5b
Minyoung <alsendrha@naver.com>
2024-10-08 13:40:23 +0900

    간격조정

--- src/components/project/Modal/ModalReview/ReviewImg.tsx
+++ src/components/project/Modal/ModalReview/ReviewImg.tsx
@@ -7,7 +7,7 @@ type ReviewImgProps = {
 
 const ReviewImg = ({ src }: ReviewImgProps) => {
   return (
-    <div className="w-[750px] max-[1500px]:w-full h-[500px] max-[1500px]:h-[550px]  max-[810px]:h-[350px] max-[610px]:h-[250px] flex-shrink-0 rounded-xl overflow-hidden relative transform transition-all duration-[0.3s] ease-out">
+    <div className="w-[750px] max-[1500px]:w-full h-[500px] max-[1500px]:h-[600px] max-[1200px]:h-[500px]  max-[900px]:h-[350px] max-[680px]:h-[250px] max-[550px]:h-[220px] flex-shrink-0 rounded-xl overflow-hidden relative transform transition-all duration-[0.3s] ease-out">
       <Image
         src={src}
         fill
--- src/components/project/ProjectContentText/ProjectContentText.tsx
+++ src/components/project/ProjectContentText/ProjectContentText.tsx
@@ -29,7 +29,7 @@ const ProjectContentText = ({
   setIsClick,
 }: ProjectContentTextProps) => {
   return (
-    <div className="px-3 ml-10 max-[1350px]:px-0 max-[550px]:px-14 w-full max-[600px]:w-[350px] max-[550px]:w-full max-[1350px]:mt-10">
+    <div className="px-3 ml-10 max-[1350px]:px-0 max-[550px]:px-2 w-full max-[600px]:w-[350px] max-[550px]:w-full max-[1350px]:mt-10">
       <div className="flex items-center mb-10">
         <p className="text-2xl font-bold text-[#5D4037]">{data.title}</p>
         <MoreButton
